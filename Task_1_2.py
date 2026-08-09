2a71c4d884f6ff4d534f972272e6961606d5ef1b
HARSH070105 <harsh.kapoor@research.iiit.ac.in>
2026-08-10 01:30:13 +0530

    Assignment 0 submission

--- Task_1_2.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-image_color_array = cv2.imread('input.jpg')
-image_gray_array = cv2.imread('input.jpg', cv2.IMREAD_GRAYSCALE)
-
-cv2.imwrite('output_color.jpg', image_color_array)
-cv2.imwrite('output_gray.jpg', image_gray_array)
--- Task_3.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-img = cv2.imread('input.jpg')
-
-mag = 50
-bright = np.clip(img.astype(int), 0, 255).astype(np.uint8)
-cv2.imwrite('bright_input.jpg', bright)
